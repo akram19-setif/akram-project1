6cee0f53a5fca6b87b2c1ceb441c6c258da7321f
Akram <akramdjidel00@gmail.com>
2022-08-06 20:16:24 +0100

    Update slider

--- script/scrypt.js
+++ script/scrypt.js
@@ -6,16 +6,26 @@ let bullets = document.querySelectorAll(".landing-bulls li");
 let images=document.querySelectorAll('.landing .container-img img');
 
 //Variables
-let i = 0,
+let i = 1,
 time = 3500,size=images[0].clientWidth,counter=0;
 
 
 
 right.addEventListener('click', function(){
-  if(counter<images.length-1){
+  if(counter<images.length -1){
+    console.log(counter);
   imgContainer.style.transition="1s ease-out";
   counter++;
-  imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;}
+  imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;
+  bullets.forEach((ele) => ele.classList.remove("active"));
+  if(i<2){
+    i++;
+  bullets[i].classList.add("active");
+}else{
+  i=0;
+  bullets[i].classList.add("active");
+}
+}
   else{
   counter=0;
   imgContainer.style.transition="none";
@@ -28,6 +38,15 @@ left.addEventListener('click',function(){
   imgContainer.style.transition="1s ease-out";
   counter--;
   imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;
+
+  bullets.forEach((ele) => ele.classList.remove("active"));
+  if(i>0){
+    i--;
+  bullets[i].classList.add("active");
+}else{
+  i=2;
+  bullets[i].classList.add("active");
+}
 } else {
   counter=images.length -2;
   imgContainer.style.transition="none";
@@ -42,8 +61,20 @@ function changeIMG() {
   if(counter<images.length-1){
     imgContainer.style.transition="1s ease-out";
     counter++;
-    imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;}
+    imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;
+    bullets.forEach((ele) => ele.classList.remove("active"));
+    if(i<2){
+    i++;
+    bullets[i].classList.add("active");
+  }else{
+    i=0;
+    bullets[i].classList.add("active");
+  }
+  }
     else{
+    i=1;
+    bullets.forEach((ele) => ele.classList.remove("active"));
+    bullets[i].classList.add("active");
     counter=0;
     imgContainer.style.transition="none";
     imgContainer.style.transform=`translateX(`+ ( -size * counter) +`px)`;
